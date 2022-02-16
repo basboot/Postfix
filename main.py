57c722cb04604e3268fe89cfd9327eec6e50e774
Bas Boot <b.boot@student.tudelft.nl>
2022-02-16 14:04:49 +0100

    Cleanup

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from typing import Callable
 
 
 @dataclass
-class Precedence:
-    priority: int
-    operation: Callable[[deque, deque], None]
-    ltr: bool = True
+class Operator:
+    priority: int  # operator priority (higher will be performed first)
+    operation: Callable[[deque, deque], None]  # function to perform operation (number_stack, operation_stack)
+    ltr: bool = True  # associativity of the operator (left-to-right, right-to-left)
 
 
+# Functions which perform the action for an operator using the number_stack for input and output
+# The operation stack parameter is only needed for the parentheses
 def add(numbers, operations):
     a = numbers.pop()
     b = numbers.pop()
@@ -21,10 +23,12 @@ def sub(numbers, operations):
     b = numbers.pop()
     numbers.append(b - a)
 
+
 def multiply(numbers, operations):
     a = numbers.pop()
     b = numbers.pop()
     numbers.append(b * a)
+
 
 def divide(numbers, operations):
     a = numbers.pop()
@@ -36,6 +40,7 @@ def remainder(numbers, operations):
     a = numbers.pop()
     b = numbers.pop()
     numbers.append(b % a)
+
 
 def power(numbers, operations):
     a = numbers.pop()
@@ -53,26 +58,22 @@ def right_parenthesis(numbers, operations):
     operation = operator_stack.pop()
 
     while operation != "(":
-        print("perform within ()")
-        print(len(operator_stack))
         operators[operation].operation(number_stack, operator_stack)
         operation = operator_stack.pop()
 
+
 # https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
 operators = {
-    '+': Precedence(12, add, True),
-    '-': Precedence(12, sub, True),
-    '*': Precedence(13, multiply, True),
-    '/': Precedence(13, divide, True),
-    '%': Precedence(13, remainder, True),
-    '^': Precedence(14, power, False),
-    '(': Precedence(0, left_parenthesis, True),  # Special case, not a real operator, set to 0 so next operator will always have higher prio, True => n/a
-    ')': Precedence(19, right_parenthesis, True)
+    '+': Operator(12, add, True),
+    '-': Operator(12, sub, True),
+    '*': Operator(13, multiply, True),
+    '/': Operator(13, divide, True),
+    '%': Operator(13, remainder, True),
+    '^': Operator(14, power, False),
+    # Special case, not a real operator, set to 0 so next operator will always have higher precendence
+    '(': Operator(0, left_parenthesis, True),
+    ')': Operator(19, right_parenthesis, True)
 }
-
-operator_stack = deque()
-number_stack = deque()
-
 
 def get_next_syllable(input_string):
     for c in input_string:
@@ -83,7 +84,7 @@ def get_next_syllable(input_string):
         yield c, not c.isnumeric()
 
 
-def has_priority(operator1, operator2):
+def has_precendence(operator2, operator1):
     # operator1 has priority over operator2 if its priority is higher,
     # or if they have the same priority and their associativity is ltr
     return operators[operator1].priority <= operators[operator2].priority if operators[operator1].ltr \
@@ -93,39 +94,30 @@ def has_priority(operator1, operator2):
 if __name__ == '__main__':
     print("Infix 2 Postfix")
 
-    input_string = "2/1/5"
+    operator_stack = deque()
+    number_stack = deque()
+
+    input_string = "((((((2+7)*8*7)-3-9)*(9-2)+9*6)*((2-3)+5-7)-4+3)*(((8-6)-4+3)-7-2)*(1-1)*5+4)*((((((6+8)%9-6)%8%4)%(3%8)-1-5)+((6+2)%9+1)+7-1)-(((7%8)-4-9)-7-5)-(4+7)+6-3)*((((((3+7)-1*5)+3-2)*(6-5)*9-2)*((9-7)*5-4)-9+9)+(((4*7)*1/7)+9*3)+(6-4)*3/1+2^3^2+2^3^2)/((((((5+5)/5-5)-5/5)+(5-5)-5*5)+((5/5)+5/5)/5*5)*(((5+5)-5-5)+5+5)*(5-5)/5+5)/((((((2+1)/3+3)/1+2)+(2+1)+2+1)/((2/1)+3/3)+2+3)/(((3+1)/2+1)/2+3)/(1/2)+2+1)/((((((8+9)%8%4)%8%1)+(1%6)%4+8)+((4%2)+8+8)%1+2)+(((1%1)+4+8)+6+5)%(2%7)+1%5)/((((9+9)-8-9)+8-8)*(9-8)+9*8)"
 
     for value, is_operator in get_next_syllable(input_string):
-        print("NEXT: ", value, is_operator)
         if is_operator:
-            # if operator has precedence add it to the stack
-            # else execute operators from the stack until it has precedence
-            # TODO: fit ltr vs rtl
+            # left parenthesis are only used for grouping. just store and 'wait' for the right parenthesis
             if value == "(":
-                print("store left bracket")
                 operator_stack.append(value)
-                print(operator_stack)
                 continue
 
-            while len(operator_stack) > 0 and has_priority(value, operator_stack[-1]):
-                print("perform operator")
-                print(len(operator_stack))
+            # if the operator on the stack has precedence over this operator it needs to be
+            # executed before adding the new operator to the stack
+            while len(operator_stack) > 0 and has_precendence(operator_stack[-1], value):
                 operation = operators[operator_stack.pop()]
                 operation.operation(number_stack, operator_stack)
-            else:
-                print("store operator")
-                operator_stack.append(value)
+
+            operator_stack.append(value)
         else:
-            print("store number")
             number_stack.append(int(value))
 
-
-    print(operator_stack)
-    print(number_stack)
-
+    # execute remaining operators on the stack
     while len(operator_stack) > 0:
-        print("perform remaining")
-        print(len(operator_stack))
         operation = operators[operator_stack.pop()]
         operation.operation(number_stack, operator_stack)
 
