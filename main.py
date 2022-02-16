8c6e3b6d0fc8a9aed2e4f592c166bb0bb77fb594
Bas Boot <b.boot@student.tudelft.nl>
2022-02-16 13:42:36 +0100

    Add associativity rtl for ^

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def add(numbers, operations):
     b = numbers.pop()
     numbers.append(b + a)
 
+
 def sub(numbers, operations):
     a = numbers.pop()
     b = numbers.pop()
@@ -30,6 +31,7 @@ def divide(numbers, operations):
     b = numbers.pop()
     numbers.append(b / a)
 
+
 def remainder(numbers, operations):
     a = numbers.pop()
     b = numbers.pop()
@@ -40,9 +42,11 @@ def power(numbers, operations):
     b = numbers.pop()
     numbers.append(b ** a)
 
+
 def left_parenthesis(numbers, operations):
-    # do nothing
+    # do nothing, wait for right parenthesis
     pass
+
 
 def right_parenthesis(numbers, operations):
     # perform all operations since the last left parenthesis
@@ -62,7 +66,7 @@ operators = {
     '/': Precedence(13, divide, True),
     '%': Precedence(13, remainder, True),
     '^': Precedence(14, power, False),
-    '(': Precedence(0, left_parenthesis, True),  # Special case, True => n/a
+    '(': Precedence(0, left_parenthesis, True),  # Special case, not a real operator, set to 0 so next operator will always have higher prio, True => n/a
     ')': Precedence(19, right_parenthesis, True)
 }
 
@@ -79,14 +83,17 @@ def get_next_syllable(input_string):
         yield c, not c.isnumeric()
 
 
-def perform_operation():
-    pass
+def has_priority(operator1, operator2):
+    # operator1 has priority over operator2 if its priority is higher,
+    # or if they have the same priority and their associativity is ltr
+    return operators[operator1].priority <= operators[operator2].priority if operators[operator1].ltr \
+        else operators[operator1].priority < operators[operator2].priority
 
 
 if __name__ == '__main__':
     print("Infix 2 Postfix")
 
-    input_string = "(1 + 2) * (3 + 1)"
+    input_string = "2/1/5"
 
     for value, is_operator in get_next_syllable(input_string):
         print("NEXT: ", value, is_operator)
@@ -100,7 +107,7 @@ if __name__ == '__main__':
                 print(operator_stack)
                 continue
 
-            while len(operator_stack) > 0 and (operators[value].priority <= operators[operator_stack[-1]].priority and operator_stack[-1] != "("):
+            while len(operator_stack) > 0 and has_priority(value, operator_stack[-1]):
                 print("perform operator")
                 print(len(operator_stack))
                 operation = operators[operator_stack.pop()]
